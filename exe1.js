1f06269de4194d5e80baf1ab8692789c3b199a38
prakash <duraprakash141@gmail.com>
2024-12-06 14:34:39 +0545

    feat: user obj des add

--- exe1.js
+++ exe1.js
@@ -14,7 +14,7 @@ const user = {
 }
 
 console.log(
-    `Name: ` + `${user.name},\n`,
+    `User Detail Information\nName: ` + `${user.name},\n`,
     `Gender: ` + `${user.gender},\n`,
     `Address\nCountry:` + `${user.address.country},\n`,
     `Provience: ` + `${user.address.provience},\n`,
@@ -23,3 +23,24 @@ console.log(
     `Longitute: ` + `${user.address.geo.lng},\n`,
 );
 
+
+// 1. Destruction
+// 1.1 Object desctructuring
+const {name, gender, address} = user; // now object element can be accessed without using user
+const userObjDest = { 
+    name: 'Sunila', 
+    gender: 'Female', 
+    address: {
+        country: 'Nepal',
+        provience: address.provience, // user.address.provience not need 
+        vdc_municipality: address.vdc_municipality,
+        geo: {
+            lat: address.geo.lat,
+            lng: address.geo.lng
+        }
+    },
+    contact: '9856256341'
+};
+console.log(userObjDest);
+
+// 1.2 Renaming
